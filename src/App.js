14362f13f9997d052472b402d54f77e7e963c15c
bhavik8455 <solankibhavik92@gmail.com>
2024-07-25 00:10:08 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import Counter from './components/button';
+import logo from './logo.svg';
 import './App.css';
-import { useContext } from 'react';
-import { Countercontext } from "./context/Counter";
-
 
 function App() {
-
-  const CounterState = useContext(Countercontext);
   return (
     <div className="App">
-
-      <h1>Counter is {CounterState.count}</h1>
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
-
-
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
